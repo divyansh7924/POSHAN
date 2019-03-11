bd2e37ed4bf1591e4a9e6574b6be14bddd5509c0
divyansh7924 <guptadivyansh49@gmail.com>
2019-03-12 00:11:23 +0530

    RCR ACTIVITY REGISTRATION UPDATE

--- app/src/main/java/com/example/sihtry1/RCRRegisterActivity.java
+++ app/src/main/java/com/example/sihtry1/RCRRegisterActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.app.ProgressDialog;
@@ -19,15 +20,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -38,11 +45,15 @@ public class RCRRegisterActivity extends AppCompatActivity {
 
     private Button submit;
     private Button browse;
+    public FirebaseFirestore db;
+    private Spinner sp_state;
     private StorageReference mStorageRef;
     private static final int PICK_PDF_REQUEST = 234;
     private Uri filepath;
-    private EditText et_bed_count, et_bed_vacant, et_title, et_address, et_city, et_state, et_pincode, et_phone, et_reg_num;
+    private EditText et_bed_count, et_bed_vacant, et_title, et_address, et_city, et_pincode, et_phone, et_reg_num;
     private Uri downloadUri;
+    ArrayList<String> states = new ArrayList<String>(25);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,18 +65,49 @@ public class RCRRegisterActivity extends AppCompatActivity {
         et_title = (EditText) findViewById(R.id.rcr_reg_et_title);
         et_address = (EditText) findViewById(R.id.rcr_reg_et_add);
         et_city = (EditText) findViewById(R.id.rcr_reg_et_city);
-        et_state = (EditText) findViewById(R.id.rcr_reg_et_state);
+        sp_state = (Spinner) findViewById(R.id.rcr_reg_et_state);
         et_pincode = (EditText) findViewById(R.id.rcr_reg_et_pincode);
         et_phone = (EditText) findViewById(R.id.rcr_reg_et_phone);
         et_reg_num = (EditText) findViewById(R.id.rcr_reg_et_reg_num);
 
         browse = (Button) findViewById(R.id.rcr_reg_doc);
+        db = FirebaseFirestore.getInstance();
         browse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 browsefile();
             }
         });
+
+        db.collection("States")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task< QuerySnapshot > task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document: task.getResult()) {
+                                Log.v("FIRESTOREEE", document.getId() + " => " + document.get("state"));
+
+
+                                states.add((String) document.get("state"));
+
+
+
+
+
+                            }
+                            final List<String> statesList = new ArrayList<>(states);
+
+                            // Initializing an ArrayAdapter
+                            final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(RCRRegisterActivity.this,R.layout.spinner_item,statesList);
+
+                            spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+                            sp_state.setAdapter(spinnerArrayAdapter);
+                        } else {
+                            Log.v("FIRESTOREEE WARNING", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,7 +156,7 @@ public class RCRRegisterActivity extends AppCompatActivity {
                         String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
                         IMainActivity iMainActivity = new IMainActivity();
                         iMainActivity.createNewRCR(getApplicationContext(), userId, et_title.getText().toString(),
-                                downloadUri.toString(), et_reg_num.getText().toString(), et_address.getText().toString(), et_state.getText().toString(),
+                                downloadUri.toString(), et_reg_num.getText().toString(), et_address.getText().toString(), sp_state.getSelectedItem().toString(),
                                 et_city.getText().toString(), Integer.parseInt(et_pincode.getText().toString()), et_phone.getText().toString(), userEmail, false);
                     } else {
                         Toast.makeText(getApplicationContext(), "upload failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -123,5 +165,4 @@ public class RCRRegisterActivity extends AppCompatActivity {
             });
         }
     }
-
 }
