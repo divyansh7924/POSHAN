2ce6f7af82891993bbebfe52f7aa41b556e76705
divyansh7924 <guptadivyansh49@gmail.com>
2019-03-26 00:11:42 +0530

    NRC list now shows distance to the nrc center

--- app/src/main/java/com/example/sihtry1/NrcListAdapter.java
+++ app/src/main/java/com/example/sihtry1/NrcListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.sihtry1;
 
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -7,15 +8,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sihtry1.models.NRC;
 import com.example.sihtry1.models.RCR;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
 
 public class NrcListAdapter extends FirestoreRecyclerAdapter<NRC, NrcListAdapter.NrcHolder> {
     private OnItemClickListener listener;
+    FirebaseFirestore db;
+    CollectionReference rcrRef;
+    String userId;
+    RCR rcr = null;
+    DocumentReference docRef;
 
 
     public NrcListAdapter(@NonNull FirestoreRecyclerOptions<NRC> options) {
@@ -24,9 +42,40 @@ public class NrcListAdapter extends FirestoreRecyclerAdapter<NRC, NrcListAdapter
 
 
     @Override
-    protected void onBindViewHolder(@NonNull NrcListAdapter.NrcHolder holder, int position, @NonNull NRC model) {
+    protected void onBindViewHolder(@NonNull final NrcListAdapter.NrcHolder holder, int position, @NonNull NRC model) {
         holder.textViewnrctitle.setText(model.getTitle());
         holder.textviewaddress.setText(model.getAddress());
+        final Double lon = model.getLon();
+        final Double lat = model.getLat();
+        db = FirebaseFirestore.getInstance();
+        rcrRef = db.collection("rcr");
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        Query query = rcrRef.whereEqualTo("user_id", userId);
+
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                        rcr = documentSnapshot.toObject(RCR.class);
+                        docRef = documentSnapshot.getReference();
+                    }
+                    Double lata = rcr.getLat();
+                    Double lona = rcr.getLon();
+                    Location loc1 = new Location("");
+                    loc1.setLatitude(lata);
+                    loc1.setLongitude(lona);
+
+                    Location loc2 = new Location("");
+                    loc2.setLatitude(lat);
+                    loc2.setLongitude(lon);
+                    float distanceInMeters = loc1.distanceTo(loc2) / 1000;
+                    holder.textviewdistance.setText(String.valueOf(distanceInMeters)+" Km");
+                } else {
+//                    Toast.makeText(getApplicationContext(), "NRC not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @NonNull
@@ -39,14 +88,16 @@ public class NrcListAdapter extends FirestoreRecyclerAdapter<NRC, NrcListAdapter
     }
 
 
-    class NrcHolder extends RecyclerView.ViewHolder{
+    class NrcHolder extends RecyclerView.ViewHolder {
         TextView textViewnrctitle;
         TextView textviewaddress;
+        TextView textviewdistance;
 
         public NrcHolder(@NonNull View itemView) {
             super(itemView);
             textViewnrctitle = itemView.findViewById(R.id.textviewnrctitle);
             textviewaddress = itemView.findViewById(R.id.textviewaddress);
+            textviewdistance = itemView.findViewById(R.id.textviewdistance);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -59,10 +110,12 @@ public class NrcListAdapter extends FirestoreRecyclerAdapter<NRC, NrcListAdapter
             });
         }
     }
-    public interface OnItemClickListener{
+
+    public interface OnItemClickListener {
         void onItemClick(DocumentSnapshot documentSnapshot, int position);
     }
-    public void setOnItemClickListener(OnItemClickListener listener){
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
         this.listener = listener;
     }
 }
