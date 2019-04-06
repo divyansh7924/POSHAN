6eb1cd2e05aef9b8a8a1348e6f2bc277a42db7d8
divyansh7924 <guptadivyansh49@gmail.com>
2019-04-06 12:40:27 +0530

    pastrecordnrc referral added

--- app/src/main/java/com/example/sihtry1/PastRecordsNrcActivity.java
+++ app/src/main/java/com/example/sihtry1/PastRecordsNrcActivity.java
@@ -1,13 +1,69 @@
 package com.example.sihtry1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.sihtry1.models.PastRecord;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 
 public class PastRecordsNrcActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference childref = db.collection("PastRecord");
+
+    private PastRecordsNrcAdapter adapter;
+    String profileselected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_records_nrc);
+
+        setupRecyclerView();
+        adapter.setOnItemClickListener(new PastRecordsNrcAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                profileselected = documentSnapshot.getId();
+            }
+        });
     }
+
+    public void setupRecyclerView() {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        Query query = childref.whereEqualTo("nrc_id", userId);
+        FirestoreRecyclerOptions<PastRecord> options = new FirestoreRecyclerOptions.Builder<PastRecord>()
+                .setQuery(query, PastRecord.class)
+                .build();
+
+        adapter = new PastRecordsNrcAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerviewnrcpastrecords);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
 }
+
